177de3834c1cb6fd45b8eb4669600980ed6facaf
Iuliia Bolgova <163222078+IuliiaBolgova@users.noreply.github.com>
2024-09-23 00:04:11 -0700

    Initial commit

--- test_chatbot.py
+++ test_chatbot.py
@@ -1,5 +1,6 @@
 import requests
 
 # Отправляем POST-запрос на сервер Flask
-response = requests.post('http://127.0.0.1:5000/chat', json={'message': 'I would like to take a mini photosession, could you give me more info for this packege?'})
+response = requests.post('http://127.0.0.1:5001/chat', json={'message': 'I would like to take a mini photosession, could you give me more info for this package?'})
+
 print(response.json())
